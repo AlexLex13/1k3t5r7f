7c7a2958ca3e1187b22589d23b652df928e29ab4
Alexey <suxovarov.alexey@gmail.com>
2022-07-19 18:41:14 +0300

    srpt.js

--- anyg666/inputX/views.py
+++ anyg666/inputX/views.py
@@ -7,11 +7,11 @@ from .models import Input
 
 class InputListView(ListView):
     model = Input
-    template_name = 'input_list.html'
+    template_name = 'inputX/input_list.html'
 
 
 class InputAddView(TemplateView):
-    template_name = "add_input.html"
+    template_name = 'inputX/add_input.html'
 
     # Define method to handle GET request
     def get(self, *args, **kwargs):
