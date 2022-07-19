80c6b2aada1db2af019274b17a2cb4a5cd14f0a4
Alexey <suxovarov.alexey@gmail.com>
2022-07-19 17:41:03 +0300

    InputAddView.post

--- anyg666/inputX/views.py
+++ anyg666/inputX/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic import ListView, TemplateView
 from .forms import InputFormSet
 from .models import Input
@@ -15,4 +17,14 @@ class InputAddView(TemplateView):
     def get(self, *args, **kwargs):
         # Create an instance of the formset
         formset = InputFormSet(queryset=Input.objects.none())
+        return self.render_to_response({'input_formset': formset})
+
+    def post(self, *args, **kwargs):
+        formset = InputFormSet(data=self.request.POST)
+
+        # Check if submitted forms are valid
+        if formset.is_valid():
+            formset.save()
+            return redirect(reverse_lazy("input_list"))
+
         return self.render_to_response({'input_formset': formset})
