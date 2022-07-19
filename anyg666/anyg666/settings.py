45ad27ed34577e1bbe1f1c6aec2ee9635399b22e
Alexey <suxovarov.alexey@gmail.com>
2022-07-19 16:14:26 +0300

    postgres db settings

--- anyg666/anyg666/settings.py
+++ anyg666/anyg666/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'anyg666.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': '2003ktrf10',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
