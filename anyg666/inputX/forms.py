4e4e6449c79de929eb4abf0be53798b706c4da0e
Alexey <suxovarov.alexey@gmail.com>
2022-07-20 14:48:11 +0300

    done

--- anyg666/inputX/forms.py
+++ anyg666/inputX/forms.py
@@ -4,8 +4,7 @@ from .models import Input
 
 
 class InputForm(forms.Form):
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}),
-                              label="Контент", empty_value="null")
+    content = forms.CharField(label="Input", empty_value="null")
 
 InputFormSet = formset_factory(
     InputForm, extra=1
